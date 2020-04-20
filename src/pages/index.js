dded66a08144239dadf610a2bd2ac29b8cde22cb
Daria Lykova <irish@SE-DARIALY.nepa.local>
2020-04-20 12:08:50 +0200

    Fixes bundle errors due to lost code snippet in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import L, { latLng, divIcon } from 'leaflet';
+import L from 'leaflet';
 import axios from 'axios';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
-import { array } from 'prop-types';
-
 
 const LOCATION = {
   lat: 0,
   lng: 0
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
-
+const DEFAULT_ZOOM = 2;
 
 const IndexPage = () => {
-
 
   /**
    * mapEffect
@@ -30,14 +27,15 @@ const IndexPage = () => {
 
     try {
       response = await axios.get('https://corona.lmao.ninja/v2/countries');
-    } catch (e) {
+    } catch(e) {
       console.log(`Failed to fetch countries: ${e.message}`, e);
       return;
     }
-    const { data = [] } = response;
-    const hasData = Array.isArray(data) && array.length > 0;
 
-    if (!hasData) return;
+    const { data = [] } = response;
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if ( !hasData ) return;
 
     const geoJson = {
       type: 'FeatureCollection',
@@ -51,16 +49,17 @@ const IndexPage = () => {
           },
           geometry: {
             type: 'Point',
-            coordinates: [lng, lat]
+            coordinates: [ lng, lat ]
           }
         }
       })
     }
+
     const geoJsonLayers = new L.GeoJSON(geoJson, {
       pointToLayer: (feature = {}, latlng) => {
         const { properties = {} } = feature;
         let updatedFormatted;
-        let caseString;
+        let casesString;
 
         const {
           country,
@@ -70,40 +69,69 @@ const IndexPage = () => {
           recovered
         } = properties
 
-        caseString = `${cases}`;
+        casesString = `${cases}`;
 
-        if (cases > 1000) {
+        if ( cases > 1000 ) {
           casesString = `${casesString.slice(0, -3)}k+`
         }
 
-        if (updated) {
+        if ( updated ) {
           updatedFormatted = new Date(updated).toLocaleString();
         }
+
         const html = `
           <span class="icon-marker">
             <span class="icon-marker-tooltip">
               <h2>${country}</h2>
               <ul>
-                <li><strong>Confirmed:</strong> ${cases}
-                <li><strong>Deaths:</strong> ${deaths}
-                <li><strong>Recovered:</strong> ${recovered}
-                <li><strong>Last Updated:</strong> ${updatedFormatted}
-              </ul> 
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
             </span>
-            ${ casesString}
+            ${ casesString }
           </span>
-          `;
+        `;
 
-        return L.marker(latlng, {
+        return L.marker( latlng, {
           icon: L.divIcon({
             className: 'icon',
             html
           }),
           riseOnHover: true
-        })
+        });
       }
-    })
+    });
+
+    geoJsonLayers.addTo(map)
   }
-  geoJsonLayers.addTo(map)
-}
+
+  const mapSettings = {
+    center: CENTER,
+    defaultBaseMap: 'OpenStreetMap',
+    zoom: DEFAULT_ZOOM,
+    mapEffect
+  };
+
+  return (
+    <Layout pageName="home">
+      <Helmet>
+        <title>Home Page</title>
+      </Helmet>
+
+      <Map {...mapSettings} />
+
+      <Container type="content" className="text-center home-start">
+        <h2>Still Getting Started?</h2>
+        <p>Run the following in your terminal!</p>
+        <pre>
+          <code>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</code>
+        </pre>
+        <p className="note">Note: Gatsby CLI required globally for the above command</p>
+      </Container>
+    </Layout>
+  );
+};
+
 export default IndexPage;
